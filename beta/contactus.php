9bca2fe2212f6e4cb67dd926baf66f749469031e
suryo <suryoatm@gmail.com>
2024-04-04 08:41:34 +0700

    add assets

---
+++ beta/contactus.php
@@ -0,0 +1,149 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Solutions For Every Industries"/>
+    <meta property="og:title" content="SDA Fluid Power" />
+    <meta property="og:image" content="assets/img/redtop.png">
+    <meta property="og:site_name" content="sda.id" />
+
+    <title>SDA Fluid Power</title>
+    <link rel="icon" href="assets/img/redtop.png">
+    <link rel="stylesheet" href="assets/bootstrap-5.3.0-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/bootstrap-icons-1.10.3/bootstrap-icons.css">
+    <link rel="stylesheet" href="assets/fonts/HelveticaNeue.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+
+<body>
+
+    <!-- NAVBAR -->
+    <div class="sticky-top">
+        <?php include 'navbar.php'; ?>
+    </div>
+
+    <main class="wrapper">
+
+        <section class="text-white py-0">
+            <img src="assets/img/contactus.png" class="img-fluid w-100">
+            <div class="row py-lg-4 py-2 position-relative" style="background-color:#c70023;"></div>
+            <div class="container d-none">
+                <header class="text-center mb-5">
+                    <h5 class="fw-bold fs-2 text-white d-inline-block mb-0">
+                        <img src="assets/img/logo_white.svg" width="40" height="auto"> <span class="align-middle">CONTACT US</span>
+                    </h5>
+                </header>
+                <form>
+                    <div class="form-group mb-3">
+                        <label for="" class="form-label">Name</label>
+                        <input type="text" class="form-control" name="">
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="" class="form-label">Email</label>
+                        <input type="email" class="form-control" name="">
+                    </div>
+                    <div class="form-group mb-5">
+                        <label for="" class="form-label">Message</label>
+                        <textarea class="form-control" rows="6"></textarea>
+                    </div>
+                    <div class="text-center">
+                        <a class="btn bg-white fw-bold" style="color: #c70023;" href="#">SEND</a>
+                    </div>
+                </form>
+            </div>
+        </section>
+        
+        <section>
+            <div class="container">
+                <ul class="list-unstyled mb-0">
+                    <li class="d-flex flex-nowrap align-items-start mb-5">
+                        <img src="assets/img/redtop.png" width="40" height="auto" class="me-4">
+                        <div>
+                            <h5 class="fw-bold">PT. SDA INDONESIA</h5>
+                            <p class="mb-0">
+                                Komp. Margomulyo Indah I Blok A No. 7-8
+                                <br>
+                                Jl. Margomulyo Indah I, Balongsari - Tandes
+                                <br>
+                                Surabaya 60186, Jawa Timur - Indonesia
+                                <br>
+                                Telephone: +62 31 748 2190, 748 2193
+                                <br>
+                                E-mail : sda@sdaindonesia.com
+                                <br>
+                                WA : +62 81220888800
+                            </p>
+                        </div>
+                    </li>
+                    <li class="d-flex flex-nowrap align-items-start">
+                        <img src="assets/img/redtop.png" width="40" height="auto" class="me-4">
+                        <div>
+                            <h5 class="fw-bold">PT. SDA GLOBAL</h5>
+                            <p class="mb-0">
+                                Komp. Raden Saleh Permai Kav. 19-20
+                                <br>
+                                Jl. Raden Saleh No. 45
+                                <br>
+                                Surabaya 60174, Jawa Timur - Indonesia
+                                <br>
+                                Telephone: +62 31 546 8800, 546 7700
+                                <br>
+                                E-mail : toko@sdaindonesia.com 
+                                <br>
+                                WA : +62 81220888800
+                            </p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </section>
+
+        <section class="p-0 position-relative">
+            <div class="card text-bg-dark rounded-0 border-0">
+                <div class="ratio ratio-21x9 rounded-0 overflow-hidden">
+                    <img src="assets/img/banner-img.png" class="card-img rounded-0 h-auto top-50 start-50 translate-middle" alt="...">
+                </div>
+                <div class="card-img-overlay top-50 start-50 translate-middle w-100" style="bottom: unset; right: unset; z-index: 100;">
+                    <div class="container">
+                        <h5 class="card-title text-uppercase display-4">
+                            customer’s <span style="color: #c70023;">satisfaction</span>
+                            <br>
+                            is our prime focus
+                        </h5>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <style type="text/css">
+            main.wrapper section:last-of-type::before {
+                content: '';
+                width: 100%;
+                height: 100%;
+                background-color: rgba(0, 0, 0, .5);
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                z-index: 10;
+            }
+        </style>
+
+    </main>
+
+    <!-- FOOTER -->
+    <?php include 'footer.php'; ?>
+    <style type="text/css">
+        #footer {
+            margin-top: 0 !important;
+        }
+    </style>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
